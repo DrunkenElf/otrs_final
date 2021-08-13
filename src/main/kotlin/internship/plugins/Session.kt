63057d15bf730859282f17a22d9ba72500025c53
DrunkenElf <dolbanutiy@gmail.com>
2021-08-13 20:48:00 +0300

    added admin cookie

--- src/main/kotlin/internship/plugins/Session.kt
+++ src/main/kotlin/internship/plugins/Session.kt
@@ -5,11 +5,21 @@ import io.ktor.sessions.*
 
 data class UserSession(
     val customerUser: String,
-    val sessionId: String
+    val sessionId: String,
+    val interfaceSession: String
+)
+data class AdminSession(
+    val login: String,
+    val psw: String
 )
 
 fun Application.configureSession(){
     install(Sessions){
-        cookie<UserSession>("user_session")
+        cookie<UserSession>("user_session"){
+            //cookie.maxAgeInSeconds = 360
+        }
+        cookie<AdminSession>("admin_session"){
+            //cookie.maxAgeInSeconds = 360
+        }
     }
 }
